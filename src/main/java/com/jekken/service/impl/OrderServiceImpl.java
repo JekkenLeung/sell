4a620954706b8029c6548e6467dc024050818698
JekkenLeung <348427639@qq.com>
2020-03-24 23:22:37 +0800

    项目搭建

--- src/main/java/com/jekken/service/impl/OrderServiceImpl.java
+++ src/main/java/com/jekken/service/impl/OrderServiceImpl.java
@@ -1,13 +1,35 @@
 package com.jekken.service.impl;
 
+import com.jekken.component.WebSocket;
+import com.jekken.dao.OrderDetailDao;
+import com.jekken.dao.OrderMasterDao;
+import com.jekken.dto.CartDTO;
 import com.jekken.dto.OrderDTO;
+import com.jekken.enums.OrderStatusEnum;
+import com.jekken.enums.PayStatusEnum;
+import com.jekken.enums.ResultEnum;
+import com.jekken.exception.SellException;
+import com.jekken.pojo.OrderDetail;
+import com.jekken.pojo.OrderMaster;
+import com.jekken.pojo.ProductInfo;
 import com.jekken.service.OrderService;
+import com.jekken.service.PayService;
 import com.jekken.service.ProductService;
+import com.jekken.service.PushMessageService;
+import com.jekken.utils.KeyUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Create by Jekken
@@ -20,20 +42,87 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private OrderDetailDao orderDetailDao;
 
+    @Autowired
+    private OrderMasterDao orderMasterDao;
+
+    @Autowired
+    private PayService payService;
+
+    @Autowired
+    private PushMessageService pushMessageService;
+
+    @Autowired
+    private WebSocket webSocket;
 
     @Override
+    @Transactional
     public OrderDTO create(OrderDTO orderDTO) {
-        return null;
+
+        String orderId = KeyUtil.genUniqueKey();
+        BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
+
+        //1.查询商品(数量，价格)
+        for (OrderDetail orderDetail:orderDTO.getOrderDetailList()){
+            ProductInfo productInfo = productService.findOne(orderDetail.getProductId());
+            if (productInfo == null){
+                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+            //2.计算订单价格
+            orderAmount = productInfo.getProductPrice().multiply(new BigDecimal(orderDetail.getProductQuantity())).add(orderAmount);
+
+            //订单详情入库
+            orderDetail.setDetailId(KeyUtil.genUniqueKey());
+            orderDetail.setOrderId(orderId);
+            BeanUtils.copyProperties(productInfo,orderDetail);
+            orderDetailDao.save(orderDetail);
+
+        }
+
+        //3.写入订单数据库
+        OrderMaster orderMaster = new OrderMaster();
+        orderDTO.setOrderId(orderId);
+        BeanUtils.copyProperties(orderDTO,orderMaster);
+        orderMaster.setOrderAmount(orderAmount);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMasterDao.save(orderMaster);
+
+        //4.扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e->new CartDTO(e.getProductId(),e.getProductQuantity())).collect(Collectors.toList());
+        productService.decreaseStock(cartDTOList);
+
+        //发送websocket消息
+        webSocket.sendMessage(orderDTO.getOrderId());
+
+        return orderDTO;
     }
 
     @Override
     public OrderDTO findOne(String orderId) {
-        return null;
+
+        OrderMaster orderMaster = orderMasterDao.findById(orderId).orElse(null);
+        if (orderMaster ==null){
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+        List<OrderDetail> orderDetailList = orderDetailDao.findByOrderId(orderId);
+        if (CollectionUtils.isEmpty(orderDetailList)){
+            throw new SellException(ResultEnum.ORDER_DETAIL_NOT_EXIST);
+        }
+
+        OrderDTO orderDTO =new OrderDTO();
+        BeanUtils.copyProperties(orderMaster,orderDTO);
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
     }
 
     @Override
     public OrderDTO findList(String buyerOpenid, Pageable pageable) {
+        
+
         return null;
     }
 
