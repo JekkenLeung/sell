41a99fced066eb58c94aed64e2f506978f861487
JekkenLeung <348427639@qq.com>
2020-03-26 23:48:54 +0800

    项目搭建

--- src/main/java/com/jekken/service/impl/PushMessageServiceImpl.java
+++ src/main/java/com/jekken/service/impl/PushMessageServiceImpl.java
@@ -6,8 +6,6 @@ import com.jekken.service.PushMessageService;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
-import me.chanjar.weixin.mp.api.impl.WxMpTemplateMsgServiceImpl;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import org.springframework.beans.factory.annotation.Autowired;
